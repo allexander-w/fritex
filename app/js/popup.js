833640ef38cd3fb5880fd28da4bd49872e4d1923
Allexander-w <work.1793593@gmail.com>
2021-11-15 00:09:55 +0600

    [added]: burger menu, adaptive

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,26 +1,44 @@
 $(document).ready(function(){
 
-	function close() {
-		$(".popup-wrapper").addClass("dn")
+	function close(vm, expandedClass, func) {
+		$(vm).removeClass( expandedClass )
+
+		if ( func ) {
+			func()
+		}
 	}
 
-	function open( vm ) {
-		$(vm).removeClass("dn")
+	function open( vm, expandedClass, func ) {
+		$(vm).addClass( expandedClass )
+
+		if ( func ) {
+			func()
+		}
 	}
 
 	$(".popup__close").click(function() {
-		close()
+		close( $(this).attr("data-class") )
 	})
+
 	$(".popup-wrapper").click(function() {
-		close()
+		close( $(this).attr("data-class") )
+
 	}).children().click(function(e) {
 		e.stopPropagation
 		return false
 	})
 
+	
+
 	$("body").on("click", ".trigger", function(e) {
 		e.preventDefault()
-		open( $(`#${ $(this).attr("data-modal") }`) )
+		console.log("reacion")
+		open( $(`#${ $(this).attr("data-modal") }`), $(this).attr("data-class"), null )
+	})
+
+	$("body").on("click", ".trigger-close", function(e) {
+		e.preventDefault()
+		close( $(`#${ $(this).attr("data-parent-id") }`), $(this).attr("data-close"), null )
 	})
 
 
